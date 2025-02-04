b122db65fd0104d460cff7d83494f4ca9432b2c1
Samuel J. Woodward <sam@woodward.fyi>
2025-02-04 15:42:25 -0500

    better ABCs

---
+++ morsels/advanced/better_ABCs/README.md
@@ -0,0 +1,5 @@
+## better ABCs
+
+**Exercise:** https://www.pythonmorsels.com/exercises/4fefb3060b6c42ec859ae6fceff28b7d/
+
+Classes that overload the built-in isinstance and issubclass functions
---
+++ morsels/advanced/better_ABCs/betterabc.py
@@ -0,0 +1,83 @@
+from abc import ABC
+
+
+class Iterable(ABC):
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Iterable:
+            if any(
+                hasattr(B, i)
+                for B in C.__mro__
+                for i in ('__iter__', '__getitem__')
+            ):
+                return True
+        return NotImplemented
+
+
+class Mapping(ABC):
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Mapping:
+            members = ['keys', 'values', 'items', '__getitem__', '__iter__']
+            for B in C.__mro__:
+                if all(hasattr(B, i) for i in members):
+                    return True
+        return NotImplemented
+
+
+class MetaHashable(type):
+
+    def __instancecheck__(self, instance):
+        try:
+            _ = hash(instance)
+        except TypeError:
+            return False
+        else:
+            return True
+
+    def __subclasscheck__(self, instance):
+        for B in instance.__mro__:
+            if '__hash__' in B.__dict__:
+                if B.__dict__['__hash__'] is None:
+                    return False
+        return True
+
+
+class Hashable(metaclass=MetaHashable):
+    pass
+
+
+class _ImmutableSequence(ABC):
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is ImmutableSequence:
+            if any(hasattr(B, '__setitem__') for B in C.__mro__):
+                if any(isinstance(B, (set, tuple)) for B in C.__mro__):
+                    breakpoint()
+                return False
+        return NotImplemented
+
+
+class MetaImmutableSequence(type):
+
+    def __instancecheck__(self, instance):
+        if hasattr(instance, '__setitem__'):
+            return False
+        try:
+            _ = hash(instance)
+        except TypeError:
+            return False
+        else:
+            return True
+
+    def __subclasscheck__(self, instance):
+        if hasattr(instance, '__setitem__') or issubclass(instance, set):
+            return False
+        return True
+
+
+class ImmutableSequence(metaclass=MetaImmutableSequence):
+    pass
