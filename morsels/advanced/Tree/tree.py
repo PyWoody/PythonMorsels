303ccca00c9f0e646d1a9065c0f87e134e3a81b4
Samuel J. Woodward <sam@woodward.fyi>
2025-03-03 15:09:24 -0500

    Tree

--- morsels/advanced/Tree/tree.py
+++ morsels/advanced/Tree/tree.py
@@ -1,97 +1,94 @@
 class Tree:
 
-    def __init__(self, nodes=None):
-        self.nodes = build(nodes if nodes is not None else {})
+    def __init__(self, initial_data=None):
+        self.data = {}
+        if initial_data is not None:
+            self.update(initial_data)
 
     def __repr__(self):
-        cls = self.__class__.__name__
-        return f'{cls}({str(self)})'
+        return str(to_dict(self.data))
 
     def __str__(self):
-        return str(to_str(self))
-
-    def __len__(self):
-        return sum(1 for i in self)
+        cls = self.__class__.__name__
+        return f'{cls}({repr(self)})'
 
     def __eq__(self, other):
         if isinstance(other, type(self)):
-            return values(self) == values(other)
+            return prune(self.data) == prune(other.data)
         return NotImplemented
 
+    def __len__(self):
+        return sum(
+            len(v) if isinstance(v, type(self)) else 1
+            for v in self.data.values() if v
+        )
+
     def __iter__(self):
-        for k, v in self.nodes.items():
+        for k, v in self.data.items():
             if v:
                 yield k
 
-    def __setitem__(self, key, value):
-        self.nodes[key] = value
-
     def __delitem__(self, key):
-        del self.nodes[key]
-
-    def __getitem__(self, key):
-        if node := self.nodes.get(key):
-            return node
-        node = type(self)()
-        self.nodes[key] = node
-        return node
-
-    def __setattr__(self, key, value):
-        if key == 'nodes':
-            return super().__setattr__(key, value)
-        return self.__setitem__(key, value)
-
-    def __getattr__(self, key):
-        if key == 'nodes':
-            return super().__getattr__(key)
-        return self.__getitem__(key)
+        del self.data[key]
 
     __delattr__ = __delitem__
 
-    def update(self, values):
-        for key, value in values.items():
-            if isinstance(value, list):
-                self.nodes[key] = value
+    def __getattr__(self, key):
+        if key == 'data':
+            return super().__getattr__(key)
+        try:
+            return self.data[key]
+        except KeyError:
+            tree = type(self)()
+            self.data[key] = tree
+            return tree
+
+    def __getitem__(self, key):
+        try:
+            return self.data[key]
+        except KeyError:
+            tree = type(self)()
+            self.data[key] = tree
+            return tree
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __setattr__(self, key, value):
+        if key == 'data':
+            super().__setattr__(key, value)
+        else:
+            self.data[key] = value
+
+    def update(self, data):
+        for k, v in data.items():
+            if k in self.data:
+                self.data[k].update(v)
             else:
-                if node := self.__getitem__(key):
-                    node.update(value)
-                else:
-                    self.__setitem__(key, type(self)(value))
+                self.data[k] = type(self)(v) if isinstance(v, dict) else v
+
+    def prune(self):
+        self.data = prune(self.data)
 
 
-def to_str(tree, output=None):
+def to_dict(data, output=None):
     if output is None:
         output = {}
-    for key, value in tree.nodes.items():
-        if isinstance(value, Tree):
-            output[key] = to_str(value)
+    for k, v in data.items():
+        if isinstance(v, Tree):
+            output[k] = to_dict(v.data)
         else:
-            output[key] = value
+            output[k] = v
     return output
 
 
-def build(nodes, output=None):
+def prune(data, output=None):
     if output is None:
         output = {}
-    for key, value in nodes.items():
-        if isinstance(value, Tree):
-            output[key] = build(value)
-        elif isinstance(value, list):
-            output[key] = value
-        else:
-            output[key] = Tree(value)
-    return output
-
-
-def values(tree, output=None):
-    if output is None:
-        output = {}
-    for key, value in tree.nodes.items():
-        if value:
-            if isinstance(value, Tree):
-                output[key] = values(value)
-            elif isinstance(value, list):
-                output[key] = value
-            else:
-                output[key] = values(Tree(value))
+    for k, v in data.items():
+        if isinstance(v, Tree):
+            if result := prune(v.data):
+                output[k] = result
+        elif v != {}:
+            output[k] = v
     return output
