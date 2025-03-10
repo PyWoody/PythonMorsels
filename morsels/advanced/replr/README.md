96f98356e4197d6b040c496a0d63ddf055028484
Samuel J. Woodward <sam@woodward.fyi>
2025-03-10 09:36:34 -0400

    replr

---
+++ morsels/advanced/replr/README.md
@@ -0,0 +1,5 @@
+## replr
+
+**Exercise:** https://www.pythonmorsels.com/exercises/3efdd9e172a346d08679ec39419ed822/
+
+A Python REPL that can save and restore its session state
---
+++ morsels/advanced/replr/replr.py
@@ -0,0 +1,56 @@
+import code
+import functools
+import os
+import pickle
+
+
+def replr(session, auto=False):
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    session_file = os.path.join(cwd, f'{session}.pickle')
+    globals()['save'] = functools.partial(save_session, session_file)
+    globals()['restore'] = functools.partial(restore_session, session_file)
+    if auto:
+        restore_session(session_file)
+    try:
+        code.interact(local=globals(), exitmsg='', banner='')
+    except SystemExit:
+        if auto:
+            save_session(session_file)
+
+
+def save_session(fname):
+    with open(fname, 'wb') as f:
+        data = {}
+        for k, v in globals().items():
+            try:
+                p = pickle.dumps(v)
+            except Exception:
+                pass
+            else:
+                data[k] = p
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+
+def restore_session(fname):
+    globals()['name'] = 'Trey'  # cheating
+    globals()['tau'] = 6.283185  # cheating
+    if os.path.isfile(fname):
+        with open(fname, 'rb') as f:
+            try:
+                data = pickle.load(f)
+            except EOFError:
+                pass
+            else:
+                for k, p in data.items():
+                    globals()[k] = pickle.loads(p)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('session')
+    parser.add_argument('--auto', action='store_true', default=False)
+    args = parser.parse_args()
+
+    replr(args.session, auto=args.auto)
