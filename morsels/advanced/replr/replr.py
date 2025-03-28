bd32b93df71b9be8455415f35d802f9b68fdd2b5
Samuel J. Woodward <sam@woodward.fyi>
2025-03-28 10:27:02 -0400

    3/3 on local

--- morsels/advanced/replr/replr.py
+++ morsels/advanced/replr/replr.py
@@ -1,48 +1,54 @@
-import code
-import functools
 import os
+import code
 import pickle
 
 
-def replr(session, auto=False):
-    cwd = os.path.abspath(os.path.dirname(__file__))
-    session_file = os.path.join(cwd, f'{session}.pickle')
-    globals()['save'] = functools.partial(save_session, session_file)
-    globals()['restore'] = functools.partial(restore_session, session_file)
+def run(session, auto=False):
+    session_file = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), f'{session}.pickle'
+    )
+    globals()['save'] = save_session(session_file)
+    globals()['restore'] = restore_session(session_file)
     if auto:
-        restore_session(session_file)
+        globals()['restore']()
     try:
-        code.interact(local=globals(), exitmsg='', banner='')
-    except SystemExit:
+        code.interact(local=globals(), banner='', exitmsg='')
+    finally:
         if auto:
-            save_session(session_file)
+            globals()['save']()
 
 
-def save_session(fname):
-    with open(fname, 'wb') as f:
-        data = {}
-        for k, v in globals().items():
-            try:
-                p = pickle.dumps(v)
-            except Exception:
-                pass
-            else:
-                data[k] = p
-        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+def save_session(session_file):
+
+    def save():
+        with open(session_file, 'wb') as f:
+            data = {}
+            for k, v in globals().items():
+                try:
+                    p = pickle.dumps(v)
+                except Exception:
+                    pass
+                else:
+                    data[k] = p
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+    return save
 
 
-def restore_session(fname):
-    globals()['name'] = 'Trey'  # cheating
-    globals()['tau'] = 6.283185  # cheating
-    if os.path.isfile(fname):
-        with open(fname, 'rb') as f:
-            try:
-                data = pickle.load(f)
-            except EOFError:
-                pass
-            else:
-                for k, p in data.items():
-                    globals()[k] = pickle.loads(p)
+def restore_session(session_file):
+
+    def restore():
+        if os.path.isfile(session_file):
+            with open(session_file, 'rb') as f:
+                try:
+                    data = pickle.load(f)
+                except EOFError:
+                    pass
+                else:
+                    for k, p in data.items():
+                        globals()[k] = pickle.loads(p)
+
+    return restore
 
 
 if __name__ == '__main__':
@@ -53,4 +59,4 @@ if __name__ == '__main__':
     parser.add_argument('--auto', action='store_true', default=False)
     args = parser.parse_args()
 
-    replr(args.session, auto=args.auto)
+    run(args.session, auto=args.auto)
