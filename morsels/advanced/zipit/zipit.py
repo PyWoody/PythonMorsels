f7bbd153709fe4f0dcadb1f5b82af0feee86e331
Samuel J. Woodward <sam@woodward.fyi>
2025-04-07 15:30:52 -0400

    zipit

--- morsels/advanced/zipit/zipit.py
+++ morsels/advanced/zipit/zipit.py
@@ -1,49 +1,44 @@
 import os
 
-from zipfile import ZIP_DEFLATED, ZIP_STORED, ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED, ZIP_STORED
 
 
 def zipit(path, deflate=False, as_main=None):
     compression = ZIP_DEFLATED if deflate else ZIP_STORED
     path = os.path.abspath(path)
     project = find_project_name(path)
-    fname = f'{project}.zip'
-    zip_output_path = os.path.join(os.path.dirname(path), fname)
-    with ZipFile(zip_output_path, mode='w', compression=compression) as z:
+    archive_name = os.path.join(os.path.dirname(path), f'{project}.zip')
+    with ZipFile(archive_name, mode='w', compression=compression) as z:
         if as_main:
             _ = z.writestr('__main__.py', as_python(as_main))
-        for src_path, out_path in find_project_files(project, path):
-            if not src_path:
-                _ = z.mkdir(out_path)
-            else:
-                _ = z.write(src_path, out_path)
+        if os.path.isfile(path):
+            _ = z.write(path, os.path.split(path)[1])
+        else:
+            _ = z.mkdir(project)
+            for root, dirs, files in os.walk(path):
+                relpath = os.path.relpath(root, start=path)
+                relpath = relpath if relpath != '.' else ''
+                for d in dirs:
+                    _ = z.mkdir(os.path.join(project, relpath, d))
+                for f in files:
+                    if os.path.splitext(f)[1].lower() == '.py':
+                        _ = z.write(
+                            os.path.join(root, f),
+                            os.path.join(project, relpath, f)
+                        )
 
 
 def find_project_name(path):
     if os.path.isfile(path):
         return os.path.split(os.path.splitext(path)[0])[1]
-    while True:
-        if '__init__.py' in os.listdir(path):
-            path = os.path.join(path, '..')
-        else:
-            return os.path.split(path)[1]
-
-
-def find_project_files(project, path):
-    if os.path.isfile(path):
-        yield path, os.path.join(os.path.split(path)[1])
-    for root, dirs, files in os.walk(path):
-        relpath = os.path.relpath(root, start=path)
-        relpath = relpath if relpath != '.' else ''
-        yield '', os.path.join(project, relpath)
-        for f in files:
-            if os.path.splitext(f)[1].lower() == '.py':
-                yield os.path.join(root, f), os.path.join(project, relpath, f)
+    while '__init__.py' in os.listdir(path):
+        path = os.path.join(path, '..')
+    return os.path.split(path)[1]
 
 
 def as_python(command):
     modules, func = command.split(':')
-    return '\n\n'.join([f'import {modules}', f'{func}()'])
+    return f'import {modules}\n\n{func}()'
 
 
 if __name__ == '__main__':
@@ -51,7 +46,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('cwd')
-    parser.add_argument('--main', '-m')
+    parser.add_argument('-m', '--main')
     parser.add_argument(
         '-0', default=True, action='store_false', dest='deflate'
     )
